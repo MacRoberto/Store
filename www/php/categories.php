4daef02315edc80ac1456413e8d455e7ddcafd05
MacRoberto <roberto-may-c@outlook.es>
2026-08-04 00:39:53 -0500

    ValidationsCategories

--- www/php/categories.php
+++ www/php/categories.php
@@ -1,14 +1,17 @@
 <?php
 require_once "../src/functions.php";
-//Se reciben los parametros
-$_get = json_decode(file_get_contents("php://input"), true);
+require_once __DIR__ . '/validators/validate-form.php';
+$categoriesRules = require __DIR__. '/validators/rules/categories.php';
 
-$accion = $_get['action'] ?? "";
+//Se reciben los parametros
+ $requestData = json_decode(file_get_contents("php://input"), true);
+
+$accion = $requestData['action'] ?? "";
 
 header("Content-Type: application/json");
 if($accion == "list"){
     //manda a llamar la funcion que realiza la consulta a la bd
-    $list = getAllCategories($_get);
+    $list = getAllCategories($requestData);
     //regresa la informacion solicitada
     echo json_encode($list);
 
@@ -19,25 +22,46 @@ if($accion == "list"){
     echo json_encode($list);
 
 }else if($accion == "getInfoByID"){
-    $id = $_get['id'] ?? "";
+    $id = $requestData['id'] ?? "";
     $category = getCategoryById($id);
     echo json_encode($category);
 
-}else if($accion == "save"){
+}else if($accion == "save") {
+    /*Hacer validaciones*/
+    $validation = validateForm(
+        $requestData,
+        $categoriesRules
+    );
+
+    if (!$validation['valid']) {
+        //En caso de error regresar el mensaje
+        echo json_encode(["error" => $validation["error"]['message']]);
+        exit;//detener la ejecucion del script
+    }
     $name = $_get['name'] ?? "";
     $description = $_get['description'] ?? "";
     $result = saveCategories($name, $description);
     echo json_encode($result);
 
 }else if($accion == "update"){
-    $id = $_get['id'] ?? "";
-    $name = $_get['name'] ?? "";
-    $description = $_get['description'] ?? "";
+     $validation = validateForm(
+        $requestData,
+        $categoriesRules
+    );
+
+    if (!$validation['valid']) {
+        //En caso de error regresar el mensaje
+        echo json_encode(["error" => $validation["error"]['message']]);
+        exit;//detener la ejecucion del script
+    }
+    $id =  $requestData['id'] ?? "";
+    $name = $requestData['name'] ?? "";
+    $description = $requestData['description'] ?? "";
     $result = updateCategories($id, $name, $description);
     echo json_encode($result);
 
 }else if($accion == "delete"){
-    $id = $_get['id'] ?? "";
+    $id = $requestData['id'] ?? "";
     $result = deleteCategories($id);
     echo json_encode($result);
 
